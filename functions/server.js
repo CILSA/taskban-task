e0d866d3f937e76242d14cb2f1e55d071ced149a
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2024-09-11 21:40:27 -0300

    manejo de rutas en server

--- functions/server.js
+++ functions/server.js
@@ -2,12 +2,17 @@
 import express from "express";
 import cors from "cors";
 import serverless from "serverless-http";
-import taskRoutes from "./routes/taskRoutes.js";
+import { fetchTasks, addTask } from "./controllers/taskController.js";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+const router = express.Router();
 
+
+router.get("/tasks", fetchTasks);
+router.post("/tasks", addTask);
 // Registrar las rutas de tareas
-app.use('/.netlify/functions/server', taskRoutes);
+app.use('/.netlify/functions/server', router);
 
 export const handler = serverless(app);
