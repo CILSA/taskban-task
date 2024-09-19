601854dd49b8b962e03411886ab183be696c5f36
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2024-09-19 15:20:57 -0300

    c

--- functions/localServer.js
+++ functions/localServer.js
@@ -1,7 +1,8 @@
 // // src/localServer.js
 // import express from "express";
 // import cors from "cors";
-// import taskRoutes from "./routes/taskRoutes.js";
+// import { fetchTasks, addTask, fetchTaskById, updateTaskById, deleteTaskById } from "./controllers/taskController.js";
+
 
 // const app = express();
 // app.use(cors());
@@ -9,8 +10,16 @@
 
 // const port = process.env.PORT || 4006;
 
-// // Usa las rutas para tareas
-// app.use('/api', taskRoutes);
+// const router = express.Router();
+
+// // Rutas de tareas
+// router.get("/tasks", fetchTasks); // Obtener tareas por boardId
+// router.post("/tasks", addTask); // Crear una nueva tarea
+// router.get("/tasks/:taskId", fetchTaskById); // Obtener una tarea por ID
+// router.put("/tasks/:taskId", updateTaskById); // Actualizar una tarea por ID
+// router.delete("/tasks/:taskId", deleteTaskById); // Eliminar una tarea por ID
+
+// app.use('/api', router);
 
 // // Servidor local para tareas
 // app.listen(port, () => {
--- functions/models/taskModel.js
+++ functions/models/taskModel.js
@@ -3,13 +3,13 @@ import { getFirestore, collection, getDocs, getDoc, doc, addDoc, updateDoc, dele
 
 // Configura Firebase
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-messaging-sender-id",
-    appId: "your-app-id",
-    measurementId: "your-measurement-id"
+    apiKey: "AIzaSyCx8GI5km0guJojFuOb9KDKNSclqFQBhLI",
+    authDomain: "taskban-v1.firebaseapp.com",
+    projectId: "taskban-v1",
+    storageBucket: "taskban-v1.appspot.com",
+    messagingSenderId: "774075443466",
+    appId: "1:774075443466:web:0b1ccf90595264ef8872f3",
+    measurementId: "G-1MCX6F9W86"
 };
 
 const app = initializeApp(firebaseConfig);
